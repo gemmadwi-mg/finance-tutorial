c6847662de36d48162b1c3e10b0ded0ec23cb97b
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-05-15 20:22:18 +0700

    datatable account delete edit

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,23 +1,18 @@
 "use client";
 
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
-import { useEffect, useState } from "react";
 import { useMountedState } from "react-use";
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 
 export const SheetProvider = () => {
-  // const isMounted = useMountedState();
-
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const isMounted = useMountedState();
 
   if (!isMounted) return null;
 
   return (
     <>
       <NewAccountSheet />
+      <EditAccountSheet />
     </>
   );
 };
